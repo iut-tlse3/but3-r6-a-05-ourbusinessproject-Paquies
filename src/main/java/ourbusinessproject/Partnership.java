6280740f9f968a7f83720a75f851b5e27dd09997
Paquies <paquiesther1@gmail.com>
2026-01-30 10:16:02 +0100

    fix #4.1 classe Partnership

--- src/main/java/ourbusinessproject/Partnership.java
+++ src/main/java/ourbusinessproject/Partnership.java
@@ -13,14 +13,26 @@ public class Partnership {
     @NotNull
     private Project project;
 
+    /**
+     * Set a date for the creation of the partnership
+     * @param date
+     */
     public void setCreationDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * Set an enterprise for the partnership
+     * @param enterprise
+     */
     public void setEnterprise(Enterprise enterprise) {
         this.enterprise=enterprise;
     }
 
+    /**
+     * Set the project of the partnership
+     * @param project
+     */
     public void setProject(Project project) {
         this.project=project;
     }
